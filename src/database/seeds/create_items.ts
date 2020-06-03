9f406598bd849aa83adf8b97fb0b3db98bdc9050
Gabriel <gabrielmssantiago@gmail.com>
2020-06-03 16:36:53 -0300

    papeis e papelao image alterada

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -4,7 +4,7 @@ export async function seed(knex: Knex) {
   await knex('items').insert([
     { title: 'Lampadas', image: 'lampadas.svg' },
     { title: 'Pilhas e Baterias', image: 'baterias.svg' },
-    { title: 'Papeis e Papelao', image: 'papies-papelao.svg' },
+    { title: 'Papeis e Papelao', image: 'papeis-papelao.svg' },
     { title: 'Residuos Eletronicos', image: 'eletronicos.svg' },
     { title: 'Residuos Organicos', image: 'organicos.svg' },
     { title: 'Oleo de cozinha', image: 'oleo.svg' },
