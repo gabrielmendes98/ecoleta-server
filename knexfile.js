1d240e34c6cb3e1bffd96232ee658c20a47ead08
Gabriel <gabrielmssantiago@gmail.com>
2020-06-17 15:59:25 -0300

    mudancas para deploy

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'dist', 'database', 'database.sqlite'),
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'dist', 'database', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, 'dist', 'database', 'seeds'),
+  },
+  useNullAsDefault: true,
+};
